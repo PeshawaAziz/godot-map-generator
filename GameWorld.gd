f342540398fe0f4d57f9f32647b4a7754ff4fa5d
PeshawaAziz <modern3p2017@gmail.com>
2021-02-15 10:43:18 -0500

    Setup the game window

--- GameWorld.gd
+++ GameWorld.gd
@@ -1,18 +1,36 @@
 extends Node2D
+class_name GameWorld
+
 # Procedurally generates a tile-based map with a border.
 # Right click or press enter to re-generate the map.
 
+# warning-ignore:unused_signal
+signal started
+# warning-ignore:unused_signal
+signal finished
+
+enum Cell {OBSTACLE, GROUND, OUTER}
+
+export var inner_size : = Vector2(10, 8)
+export var perimeter_size : = Vector2(1, 1)
+export (float, 0, 1) var ground_probability : = 0.1
 
 # Public variables
-
+var size := inner_size + 2 * perimeter_size
 
 # Private variables
+onready var _tile_map : TileMap = $TileMap
+var _rng := RandomNumberGenerator.new()
 
-
+func _ready() -> void:
+	setup()
 
 func setup() -> void:
 	# Sets the game window size to twice the resolution of the world.
-	return
+	var map_size_px := size * _tile_map.cell_size
+	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_KEEP, map_size_px)
+	OS.set_window_size(2* map_size_px)
+	
 
 
 func generate() -> void:
